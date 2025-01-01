594f00b19b56b7fee1096554885872c87b1cb3fc
Ramees <rameeswork@gmail.com>
2025-01-01 15:24:37 +0530

    zoommeeting 1:47:26

--- app/(call)/_layout.tsx
+++ app/(call)/_layout.tsx
@@ -1,7 +1,8 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Redirect, Tabs } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { useAuth } from "@clerk/clerk-expo";
+import { View } from "react-native";
 
 export default function CallRoutesLayout() {
   const { isSignedIn } = useAuth();
@@ -31,6 +32,48 @@ export default function CallRoutesLayout() {
             title: "All Calls",
             tabBarIcon: ({ color }) => {
               return <Ionicons name="call" size={24} color={color} />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="[id]"
+          options={{
+            title: "Start a New call",
+            unmountOnBlur: true,
+            header: () => null,
+            tabBarIcon: ({ color }) => {
+              return (
+                <View
+                  style={{
+                    position: "absolute",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    // top: -10,
+                    // left: 20,
+                    // right: 20,
+                    top: -20,
+                    left: -30,
+                    right: -30,
+                    bottom: 0,
+                    margin: "auto",
+
+                    borderRadius: 50,
+                    zIndex: 100,
+                    backgroundColor: "white",
+                    borderColor: "lightgrey",
+                    borderWidth: 0.2,
+                    borderTopWidth: 1,
+                    borderBottomWidth: 0,
+                  }}
+                >
+                  <FontAwesome
+                    name="plus-circle"
+                    size={30}
+                    color="black"
+                    style={{ zIndex: 200 }}
+                  />
+                </View>
+              );
             },
           }}
         />
--- app/(call)/index.tsx
+++ app/(call)/index.tsx
@@ -1,19 +1,46 @@
 // import { Text } from "@/components/Themed";
-import { SignedIn } from "@clerk/clerk-expo";
+import { SignedIn, useAuth } from "@clerk/clerk-expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
-import { Platform, Text, View } from "react-native";
+import { useState } from "react";
+import { Platform, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Dialog from "react-native-dialog";
 
 export default function TabOneScreen() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const { signOut } = useAuth();
   return (
-    // <SafeAreaView>
-      <View>
-        {/* <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} /> */}
-        <Text style={{marginTop:10,backgroundColor: "#5F5DEC"}}>Hello worldd</Text>
-        <SignedIn>
-          <Text>you are signed in</Text>
-        </SignedIn>
-      </View>
-    // </SafeAreaView>
+    <View>
+      {/* <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} /> */}
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          top: 20,
+          right: 20,
+          zIndex: 100,
+        }}
+        onPress={() => setDialogOpen(true)}
+      >
+        <MaterialCommunityIcons name="exit-run" size={24} color="#5F5DEC" />
+      </TouchableOpacity>
+      <Dialog.Container visible={dialogOpen}>
+        <Dialog.Title>Sign Out</Dialog.Title>
+        <Dialog.Description>Do you want to sign out?</Dialog.Description>
+        <Dialog.Button label="Cancel" onPress={() => setDialogOpen(false)} />
+        <Dialog.Button
+          label="Sign Out"
+          onPress={async () => {
+            await signOut();
+            setDialogOpen(false);
+          }}
+        />
+      </Dialog.Container>
+
+      <Text>Hello worldd</Text>
+      <SignedIn>
+        <Text>you are signed in</Text>
+      </SignedIn>
+    </View>
   );
 }
